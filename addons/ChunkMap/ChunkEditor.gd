905d736a0e05059e041145e38a937541ae65d5d9
Bryan Miller <bmiller1008@gmail.com>
2022-02-09 13:44:38 -0500

    Trimmed the tileset to only two tiles. Trying to find a bug, but have failed thus far.

--- addons/ChunkMap/ChunkEditor.gd
+++ addons/ChunkMap/ChunkEditor.gd
@@ -19,6 +19,7 @@ export (int, 1, 10) var chunk_distance : int = 4			setget set_chunk_distance
 # -------------------------------------------------------------------------
 var _chunks : Dictionary = {}
 var _active_chunk_pos : Vector2 = Vector2.ZERO
+var _ignore_chunk_set : bool = false
 
 # -------------------------------------------------------------------------
 # Onready Variables
@@ -42,6 +43,9 @@ func _ready() -> void:
 	_LoadInsideChunks(_active_chunk_pos)
 
 func _draw() -> void:
+	print("Updating")
+	for pos in _chunks.keys():
+		print("Chunk: ", _chunks[pos].node, " | Cell Count: ", _chunks[pos].node.get_used_cells().size())
 	var chunk_size : Vector2 = CHUNK_SIZE * cell_size 
 	var r : Rect2 = Rect2(_active_chunk_pos * chunk_size, chunk_size)
 	draw_rect(r, Color(0.0, 0.4, 1.0), false, 3)
@@ -55,20 +59,22 @@ func _tool_unhandled_input(event) -> bool:
 		var mpos : Vector2 = get_viewport().get_mouse_position()
 		var chunk_size = CHUNK_SIZE * cell_size
 		var chunk_pos = (mpos / chunk_size).floor()
-#		var chunk_pos = Vector2(
-#			floor(mpos.x / (CHUNK_SIZE.x * cell_size.x)),
-#			floor(mpos.y / (CHUNK_SIZE.y * cell_size.y))
 #		)
 		if chunk_pos != _active_chunk_pos:
 			if _active_chunk_pos in _chunks:
-				clear()
-				_chunks[_active_chunk_pos].modulate = Color(1,1,1,0.5)
+				print("Clearing")
+				_ClearChunkArea(_active_chunk_pos)
+				#.clear()
 			if chunk_pos in _chunks:
 				_CopyChunkToEditor(chunk_pos)
 			_active_chunk_pos = chunk_pos
-			_UnloadOutsideChunks(_active_chunk_pos)
-			_LoadInsideChunks(_active_chunk_pos)
-			update()
+			#_UnloadOutsideChunks(_active_chunk_pos)
+			#_LoadInsideChunks(_active_chunk_pos)
+			#update()
+	elif event is InputEventMouseButton:
+		if not event.pressed:
+			for pos in _chunks.keys():
+				print("Chunk: ", _chunks[pos].node, " | Cell Count: ", _chunks[pos].node.get_used_cells().size())
 	return false
 	
 # -------------------------------------------------------------------------
@@ -121,11 +127,9 @@ func _LoadInsideChunks(chunk_pos : Vector2) -> void:
 			if not cpos in _chunks:
 				var tm : TileMap = _LoadChunkAtPosition(cpos)
 				if tm != null:
-					_chunks[cpos] = {
-						"node":tm,
-						"dirty":false
-					}
+					_StoreChunkNode(cpos, tm)
 					add_child(tm)
+					tm.modulate = Color(1,1,1,0.5)
 
 func _LoadChunkAtPosition(chunk_pos : Vector2) -> TileMap:
 	if chunk_path == "":
@@ -161,16 +165,31 @@ func _SaveDirtyChunks() -> void:
 	for chunk_pos in _chunks.keys():
 		if _chunks[chunk_pos].dirty:
 			var packed : PackedScene = PackedScene.new()
-			packed.pack(_chunks[chunk_pos])
+			_chunks[chunk_pos].node.modulate = Color(1,1,1,1)
+			packed.pack(_chunks[chunk_pos].node)
 			var filepath : String = chunk_path + "/" + _GetChunkNameFromPos(chunk_pos) + ".tscn"
 			ResourceSaver.save(filepath, packed)
 			print("Saved chunk : ", filepath)
+			_chunks[chunk_pos].node.modulate = Color(1,1,1,0.5)
 			_chunks[chunk_pos].dirty = false
 
+
+func _ClearChunkArea(chunk_pos : Vector2) -> void:
+	var cpos_x : float = chunk_pos.x * CHUNK_SIZE.x
+	var cpos_y : float = chunk_pos.y * CHUNK_SIZE.y
+	_ignore_chunk_set = true
+	for y in range(cpos_y, cpos_y + CHUNK_SIZE.y):
+		for x in range(cpos_x, cpos_x + CHUNK_SIZE.x):
+			.set_cell(x, y, -1)
+	_ignore_chunk_set = false
+
+
 func _CopyChunkToEditor(chunk_pos : Vector2) -> void:
+	print("Attempting to copy chunk at: ", chunk_pos)
 	if chunk_pos in _chunks:
 		var tm : TileMap = _chunks[chunk_pos].node
 		var cells = tm.get_used_cells()
+		print("Cell count: ", cells.size())
 		for cell in cells:
 			var id = tm.get_cell(cell.x, cell.y)
 			if id != INVALID_CELL:
@@ -184,19 +203,22 @@ func _CopyChunkToEditor(chunk_pos : Vector2) -> void:
 # Public Methods
 # -------------------------------------------------------------------------
 func set_cell(x : int, y : int, tile : int, flip_x : bool = false, flip_y : bool = false, transpose : bool = false, autotile_coord : Vector2 = Vector2(0, 0)) -> void:
-	var cpos : Vector2 = (Vector2(x, y) / CHUNK_SIZE).floor()
-	if not cpos in _chunks:
-		print("Creating chunk at position: ", cpos)
-		var tm : TileMap = _CreateChunkAtPosition(cpos)
-		if not tm:
-			return
-		_StoreChunkNode(cpos, tm)
-		add_child(tm)
-	
-	if cpos in _chunks:
-		print("Setting Chunk Tile: ", cpos)
-		_chunks[cpos].node.set_cell(x, y, tile, flip_x, flip_y, transpose, autotile_coord)
-		_chunks[cpos].dirty = true
+	if not _ignore_chunk_set:
+		var cpos : Vector2 = (Vector2(x, y) / CHUNK_SIZE).floor()
+		if not cpos in _chunks:
+			#print("Creating chunk at position: ", cpos)
+			var tm : TileMap = _CreateChunkAtPosition(cpos)
+			if not tm:
+				print("Failed to create new chunk")
+				return
+			_StoreChunkNode(cpos, tm)
+			add_child(tm)
+		
+		if cpos in _chunks:
+			#print("Setting Chunk Tile: ", cpos)
+			_chunks[cpos].node.set_cell(x, y, tile, flip_x, flip_y, transpose, autotile_coord)
+			_chunks[cpos].node.modulate = Color(1,1,1,0.75)
+			_chunks[cpos].dirty = true
 	.set_cell(x, y, tile, flip_x, flip_y, transpose, autotile_coord)
 
 # -------------------------------------------------------------------------
